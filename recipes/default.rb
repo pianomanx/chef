06144138df945557fb5f5b4c6f916ff890318804
jjoyneriv <james.joyner.iv@me.com>
2019-06-03 12:58:51 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,16 +6,16 @@
 
 # Get pip from Pypa
 remote_file "#{Chef::Config[:file_cache_path]}/get-pip.py" do
-  source "https://bootstrap.pypa.io/get-pip.py"
+  source 'https://bootstrap.pypa.io/get-pip.py'
 end
 
 # Install Pip for Python 2.7.5
-execute "install pip" do
+execute 'install pip' do
   command "python #{Chef::Config[:file_cache_path]}/get-pip.py"
   live_stream true
 end
 
-# Install Pip module certifi if node is redhat 
+# Install Pip module certifi if node is redhat
 case node['platform_family']
 when 'rhel'
   execute 'install certifi' do
