211cd23c6be39f82894367090ca05c29bdc8305c
J.J. Joyner <jjoyner@oath.com>
2019-06-03 10:58:54 -0700

    added foodcritics updates

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,11 @@ description 'Installs/Configures validatepip'
 long_description 'Installs/Configures validatepip'
 version '0.1.0'
 chef_version '>= 14.3.37'
+supports 'redhat'
+
+
+opensource license supermarket
+
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -13,10 +18,14 @@ chef_version '>= 14.3.37'
 #
 # issues_url 'https://github.com/<insert_org_here>/validatepip/issues'
 
+issues_url 'https://github.com/jjoyneriv/chef/issues'
+
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/validatepip'
+
+source_url 'https://github.com/jjoyneriv/chef'
 
 depends 'poise-python'
