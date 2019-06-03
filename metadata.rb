9ec07290933419c4493f03d7d344bedf4822c9a1
J.J. Joyner <jjoyner@oath.com>
2019-06-03 11:04:37 -0700

    added foodcritics updates

--- metadata.rb
+++ metadata.rb
@@ -8,10 +8,6 @@ version '0.1.0'
 chef_version '>= 14.3.37'
 supports 'redhat'
 
-
-opensource license supermarket
-
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
